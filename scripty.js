491805350b6baf76eda7b891590a4a57b5377c7e
KSCairney <KSCairney@gmail.com>
2017-04-23 14:42:39 -0700

    red and purple

--- scripty.js
+++ scripty.js
@@ -1,32 +1,25 @@
 var creation = function(number) {
     var div = document.createElement("div");
     div.innerHTML = "";
+    div.setAttribute("class", "blank");
     document.body.appendChild(div);
 }
 
 var lines = function(number) {
-    for (var k = 1; k < 17; k++) {
+    for (var k = 1; k < 25; k++) {
         creation(k);
     }
 }
 
-var shell = function(number) {
-    var newLine = document.createElement("div");
-    newLine.setAttribute("class", "NewLine");
-    document.body.appendChild(newLine);
-    /*newLine.innerHTML = "<a href='javascript:lines();></a>";*/
-    var br = document.createElement("br");
-    document.body.appendChild(br);
-}
 
-for (var s = 1; s < 17; s++) {
-    shell(s);
-}
-
-/*
-for (var r = 1; r < 17; r++) {
-    var br = document.createElement("br");
-    document.body.appendChild(br);
+for (var r = 1; r < 25; r++) {
     lines(r);
+    var br = document.createElement("br");
+    document.body.appendChild(br);
 }
-*/
+
+$(document).ready(function(){
+                $('.blank').mouseover(function() {
+                    $(this).addClass("white").removeClass("blank");
+                 });
+            });
