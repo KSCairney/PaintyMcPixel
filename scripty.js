8684e1fdb30541202a57bb1d00336ca228db798e
KSCairney <KSCairney@gmail.com>
2017-04-23 22:41:39 -0700

    button 1

--- scripty.js
+++ scripty.js
@@ -1,25 +1,28 @@
 var creation = function(number) {
     var div = document.createElement("div");
-    div.innerHTML = "";
+    /*div.innerHTML = "";*/
     div.setAttribute("class", "blank");
     document.body.appendChild(div);
 }
 
 var lines = function(number) {
-    for (var k = 1; k < 25; k++) {
+    for (var k = 1; k < 43; k++) {
         creation(k);
     }
 }
 
 
-for (var r = 1; r < 25; r++) {
+for (var r = 1; r < 43; r++) {
     lines(r);
     var br = document.createElement("br");
     document.body.appendChild(br);
 }
 
 $(document).ready(function(){
-                $('.blank').mouseover(function() {
-                    $(this).addClass("white").removeClass("blank");
-                 });
-            });
+    $('.blank').mouseover(function() {
+        $(this).addClass("white").removeClass("blank");
+    });
+    $('.bttn').click(function() {
+        $(".white").addClass("blank").removeClass("white");
+    });
+});
